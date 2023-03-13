8ad04e14b99e8cb7ac26bc914f6a3a9176ce0bc9
Elton Sandoval Pérez <96536598+ejohnsan@users.noreply.github.com>
2023-03-13 16:33:22 -0500

    update email

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -142,8 +142,8 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = "johnsandoval69@gmail.com"
-EMAIL_HOST_PASSWORD = "qresnecqlmtvfwpl"
+EMAIL_HOST_USER = "sandops44@gmail.com"
+EMAIL_HOST_PASSWORD = "kofqdmpzwrytabwy"
 
 
 
